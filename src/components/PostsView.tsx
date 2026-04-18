7aa2a90d6f26c7281130828de4648a046d7b1607
RAFAELRGR <rafaelronaldog@gmail.com>
2026-04-18 03:12:53 -0500

    feat: add custom hook with 300ms debounce

--- src/components/PostsView.tsx
+++ src/components/PostsView.tsx
@@ -3,14 +3,16 @@
 import { useState } from "react";
 import PostsTable from "./PostsTable";
 import PostsSearch from "./PostsSearch";
+import { useDebounce } from "@/hooks/useDebounce";
 
 export default function PostsView() {
   const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 300);
 
   return (
     <div>
       <PostsSearch searchValue={search} setSearchValue={setSearch} />
-      <PostsTable search={search} />
+      <PostsTable search={debouncedSearch} />
     </div>
   );
 }
